d40796cc5fcb3854dfcb0ce215ae1775b1f8219d
Panu Lahtinen <pnuu+git@iki.fi>
2020-10-04 19:25:41 +0300

    'Finalize' the main script

--- bin/picam.py
+++ bin/picam.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 
-from piskycam import SkyCam
+import sys
+import datetime as dt
+
+import yaml
+
+from piskycam import SkyCam, sun_elevation_now
+
+
+def read_config(fname):
+    """"Read configuration file."""
+    with open(fname, 'r') as fid:
+        config = yaml.load(fid, Loader=yaml.SafeLoader)
+    return config
+
+
+def sun_is_down(lat, lon, elevation_limit):
+    """Return True if Sun is below the horizon."""
+    elevation = sun_elevation_now(lat, lon)
+    if elevation <= elevation_limit:
+        return True
+    return False
+
+
+def main():
+    """Run SkyCam."""
+    config = read_config(sys.argv[1])
+    if sun_is_down(config['latitude'], config['longitude'], config.get('sun_elevation_limit', 0.0)):
+        skycam = SkyCam(config)
+        skycam.run()
+
+
+if __name__ == "__main__":
+    main()
